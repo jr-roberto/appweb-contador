37218e740f1fc25450a5905d3a20ae632fa4d170
Roberto Gomes <91014834+jr-roberto@users.noreply.github.com>
2023-05-29 21:38:13 -0300

    Atualizacao necessaria

--- static/main.js
+++ static/main.js
@@ -1,18 +1,4 @@
-const conteudo = document.getElementById("conteudo");
-
-function iniciarContador(){
-    let i = conteudo.innerText;
-
-    if ( i !== "Iniciar" ){
-        let x = parseInt(i);
-        x++;
-        conteudo.innerHTML = `<p>${x}</p>`
-                
-    } else {
-        conteudo.innerHTML = "<p>0</p>"
-    }
-
-    console.log(i);
-
-    setTimeout( iniciarContador , 1000 );
+function agendaSite() {
+    let btnAgenda = document.getElementById("btnAgenda");
+    btnAgenda.classList.add("show");
 }
